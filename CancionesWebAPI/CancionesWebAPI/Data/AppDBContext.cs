22eb0b43064353feed06696c1c8e682674a2d63e
Marcelo Dávila <a2020110679@estudiantes.upsa.edu.bo>
2021-10-13 12:06:59 -0400

    AppDBContext creado

---
+++ CancionesWebAPI/CancionesWebAPI/Data/AppDBContext.cs
@@ -0,0 +1,20 @@
+﻿using CancionesWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CancionesWebAPI.Data
+{
+    public class AppDBContext: DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+        
+        // Agregar los modelos a utilizar
+        public DbSet<Cancion> Cancion { get; set; }
+    }
+}
